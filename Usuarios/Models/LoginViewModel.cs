0fe7e6de96a3fa52a5fa98eda67eb585034b5897
carlos bolorino <carlosbolorino@hotmail.com>
2019-01-13 19:11:57 +0100

    LoginViewModel

---
+++ Usuarios/Models/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Usuarios.Models
+{
+    public class LoginViewModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set;}
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public int Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+
+    }
+
+}
