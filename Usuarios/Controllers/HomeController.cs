4bc80605857e428b5814f7419f1b95f2ecdf0b33
carlos bolorino <carlosbolorino@hotmail.com>
2019-01-13 18:35:46 +0100

    Roles

--- Usuarios/Controllers/HomeController.cs
+++ Usuarios/Controllers/HomeController.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Usuarios.Models;
 
 namespace Usuarios.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        IServiceProvider _serviceProvider;
+        public HomeController(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await CreateRoles(_serviceProvider);
+
             return View();
         }
 
@@ -42,8 +53,30 @@ namespace Usuarios.Controllers
 
         async Task CreateRoles(IServiceProvider serviceProvider)
         {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] rolesName = { "Admin", "User" };
 
+            foreach (var item in rolesName)
+            {
+                bool roleExist = await roleManager.RoleExistsAsync(item);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+            //IdentityUser
+            var user = await userManager.FindByIdAsync("dfc2f3d7-5029-4caf-95e5-6d71b0c213a0");
 
+            try
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
--- Usuarios/Startup.cs
+++ Usuarios/Startup.cs
@@ -36,7 +36,9 @@ namespace Usuarios
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            //services.AddDefaultIdentity<IdentityUser>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
